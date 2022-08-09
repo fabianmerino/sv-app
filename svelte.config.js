e6b43e81718dd5fe322ec4f8aed039df25872079
Fabian Ary Merino Livia <fab_ary@outlook.com>
2022-08-09 14:57:07 -0500

    add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,9 @@ const config = {
 	preprocess: preprocess({ postcss: true, sass: true }),
 
 	kit: {
-		adapter: adapter(),
+		adapter: vercel({
+			split: true
+		}),
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
 		},
