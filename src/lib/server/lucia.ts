35baa57aaaf7115323593c73fa42a14cb71a2387
Fabian Ary Merino Livia <fab_ary@outlook.com>
2022-08-19 05:27:14 -0500

    roolback

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -2,13 +2,14 @@ import lucia from 'lucia-sveltekit';
 import mongooseAdapter from '@lucia-sveltekit/adapter-mongoose';
 import mongoose from 'mongoose';
 import { dev } from '$app/env';
+import { APP_SECRET, MONGODB_URI } from '$env/static/private';
 import { RefreshToken, User } from 'models/Auth';
 
 mongoose.model('user', User);
 mongoose.model('refresh_token', RefreshToken);
 
 export const auth = lucia({
-	adapter: mongooseAdapter(mongoose, import.meta.env.VITE_MONGODB_URI),
-	secret: import.meta.env.VITE_APP_SECRET,
+	adapter: mongooseAdapter(mongoose, MONGODB_URI),
+	secret: APP_SECRET,
 	env: dev ? 'DEV' : 'PROD'
 });
